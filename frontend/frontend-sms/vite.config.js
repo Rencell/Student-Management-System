eb428b5ff4c98268eb6302b99947511e5bc62157
rencell <rencelltobelonia67@gmail.com>
2025-06-04 01:24:11 +0800

    url

--- frontend/frontend-sms/vite.config.js
+++ frontend/frontend-sms/vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base : '/Student-Management-System/',
+  base : '/',
   plugins: [
     vue(),
     vueDevTools(),
