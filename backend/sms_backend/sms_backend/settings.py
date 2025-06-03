43eab4ceeabb8c8776124b2c0a03266cb2c71caa
rencell <rencelltobelonia67@gmail.com>
2025-06-04 01:30:44 +0800

    CORS

--- backend/sms_backend/sms_backend/settings.py
+++ backend/sms_backend/sms_backend/settings.py
@@ -154,7 +154,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [  
+    'https://frontend-student-management-system.onrender.com',
+    'http://localhost:5173',  
+]
+
+CORS_ALLOW_CREDENTIALS = True 
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
